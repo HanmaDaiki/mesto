5548d5fd47f46f621a3c2621b44688832f0fb3ba
HanmaDaiki <daikihanma@outlook.com>
2022-05-09 12:44:49 +0300

    feature: Логика: попап(закрытие), валидация

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,94 @@ const cardTemplate = page.querySelector('#card-template').content;
 
 const arrayCards = initialCards;
 
+const popupInputs = page.querySelectorAll('.popup__input');
+
 const profileName = page.querySelector('.profile__name');
 const profileDiscription = page.querySelector('.profile__discription');
 const buttonOpenAddCard = page.querySelector('.profile__add');
 const buttonOpenEdit = page.querySelector('.profile__edit');
 
 const popupEdit = page.querySelector('.popup_edit');
+const popupEditOverlay = popupEdit.querySelector('.popup__overlay');
 const popupEditForm = popupEdit.querySelector('.popup__form');
+const popupEditInput = popupEdit.querySelector('.popup__input');
 const popupEditInputName = popupEditForm.querySelector('.popup__input_type_name');
 const popupEditInputDiscription = popupEditForm.querySelector('.popup__input_type_discription');
 const buttonCloseEditPopup = popupEdit.querySelector('.popup__close');
 
 const popupAddCard = page.querySelector('.popup_add-card');
+const popupAddCardOverlay = popupAddCard.querySelector('.popup__overlay');
 const popupAddCardForm = popupAddCard.querySelector('.popup__form');
+const popuAddCardInput = popupAddCard.querySelector('.popup__input');
 const popupAddCardInputNameCard = popupAddCard.querySelector('.popup__input_type_name-card');
 const popupAddCardInputLinkCard = popupAddCard.querySelector('.popup__input_type_link-card');
 const buttonCloseAddCardPopup = popupAddCard.querySelector('.popup__close');
 
 const popupImage = page.querySelector('.popup_image');
+const popupImageOverlay = popupImage.querySelector('.popup__overlay');
+const popupImageInput = popupImage.querySelector('.popup__input');
 const popupImageImage = popupImage.querySelector('.popup__image');
 const popupImageTitle = popupImage.querySelector('.popup__image-title');
 const buttonCloseImagePopup = popupImage.querySelector('.popup__close');
+
+const showInputError = (formElement, inputElement, errorMessage, popupInputError) => {
+  const errorElement = formElement.querySelector(`.error-${inputElement.id}`);
+  inputElement.classList.add(popupInputError);
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement, popupInputError) => {
+  const errorElement = formElement.querySelector(`.error-${inputElement.id}`);
+  inputElement.classList.remove(popupInputError);
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement, popupInputError) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, popupInputError);
+  } else {
+    hideInputError(formElement, inputElement, popupInputError);
+  }
+};
+
+const setEventListeners = (formElement, popupInputs, popupButtonSave, popupButtonSaveInactive, popupInputError) => {
+  const inputList = Array.from(formElement.querySelectorAll(popupInputs));
+  const buttonElement = formElement.querySelector(popupButtonSave);
+  
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement, popupInputError);
+      toggleButtonState(inputList, buttonElement, popupButtonSaveInactive);
+    });
+  });
+};
+
+const enableValidation = (validate) => {
+  const formList = Array.from(document.querySelectorAll(validate.popupForm));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, validate.popupInputs, validate.popupButtonSave, validate.popupButtonSaveInactive, validate.popupInputError);
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, popupButtonSaveInactive) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(popupButtonSaveInactive)
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(popupButtonSaveInactive)
+    buttonElement.disabled = false;
+  }
+};
 
 const openPopup = (popup) => {
   popup.classList.add('popup_active');
@@ -77,6 +144,7 @@ const openImagePopup = (title, link) => {
 
   popupImageTitle.textContent = title;
 }
+
 const closeImagePopup = () => {
   closePopup(popupImage);
 }
@@ -112,14 +180,40 @@ const likeCard = (e) => {
   e.target.classList.toggle('card__like_active');
 };
 
+const closePopupEscEdit = (e) => {
+  if (e.key === 'Escape') {
+    closePopup(popupEdit);
+  }
+}
+
+const closePopupEscAddCard = (e) => {
+  if (e.key === 'Escape') {
+    closePopup(popupAddCard);
+  }
+}
+
+const closePopupEscImage = (e) => {
+  if (e.key === 'Escape') {
+    closePopup(popupImage);
+  }
+}
+
 renderCards(arrayCards);
 
 buttonOpenEdit.addEventListener('click', openEditPopup);
+popupEditOverlay.addEventListener('click', closeEditPopup);
+document.addEventListener('keydown', closePopupEscEdit);
 buttonCloseEditPopup.addEventListener('click', closeEditPopup);
 popupEditForm.addEventListener('submit', saveEditPopup);
 
 buttonOpenAddCard.addEventListener('click', openAddCardPopup);
+popupAddCardOverlay.addEventListener('click', closeAddCardPopup);
+document.addEventListener('keydown', closePopupEscAddCard);
 buttonCloseAddCardPopup.addEventListener('click', closeAddCardPopup);
 popupAddCardForm.addEventListener('submit', saveAddCardPopup);
 
+popupImageOverlay.addEventListener('click', closeImagePopup);
+document.addEventListener('keydown', closePopupEscImage);
 buttonCloseImagePopup.addEventListener('click', closeImagePopup);
+
+enableValidation(validate);
---
+++ scripts/validate.js
@@ -0,0 +1,7 @@
+const validate = {
+  popupForm: '.popup__form',
+  popupInputs: '.popup__input',
+  popupButtonSave: '.popup__save',
+  popupButtonSaveInactive: 'popup__save_inactive',
+  popupInputError: 'popup__input_error',
+};
