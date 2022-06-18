a6531abe80ce56bd873660a2017056551d8e47d3
HanmaDaiki <daikihanma@outlook.com>
2022-06-18 20:30:22 +0300

    fix: изменение механики работы метода гетИнпутВалуес.

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -1,25 +1,23 @@
 import Popup from "./Popup";
 
 export default class PopupWithForm extends Popup{
-  constructor(callBackSubmit ,popup) {
+  constructor(callBackSubmit, popup) {
     super(popup);
     this._callBackSubmit = callBackSubmit;
     this._form = this._popup.querySelector('.popup__form');
+    this._listInput = this._popup.querySelectorAll('.popup__input');
   }
 
   _getInputValues() {
-    const arrayWithInputValues = []
-    this._popup.querySelectorAll('.popup__input').forEach((item) => {
-      arrayWithInputValues.push(item.value)
+    const objWithInputValues = {};
+    this._listInput.forEach((item) => {
+      objWithInputValues[item.name] = item.value;
     });
-    return arrayWithInputValues;
+    return objWithInputValues;
   }
 
-
   setEventListeners() {
-    this._buttonClose.addEventListener('click', this.close.bind(this));
-    this._overlay.addEventListener('click', this.close.bind(this));
-    document.addEventListener('keydown', this._closeMetod);
+    super.setEventListeners();
     this._form.addEventListener('submit', () => {
       this._callBackSubmit(this._getInputValues());
       super.close();
