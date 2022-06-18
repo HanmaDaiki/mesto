9c8592f3dab7157a581adaacc0b37d689b8ecd8b
HanmaDaiki <daikihanma@outlook.com>
2022-06-18 20:30:42 +0300

    fix: изменение работы метода оупен

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -1,18 +1,16 @@
 import Popup from "./Popup";
 
 export default class PopupWithImage extends Popup {
-  constructor({title, link}, popup) {
+  constructor(popup) {
     super(popup);
-    this._title = title;
-    this._link = link;
     this._image = this._popup.querySelector(".popup__image");
     this._imageTitle = this._popup.querySelector(".popup__image-title");
   }
 
-  open() {
-    this._image.src = this._link;
-    this._image.alt = `Фото сделаное в ${this._title}`;
-    this._imageTitle.textContent = this._title;
-    this._popup.classList.add("popup_active");
+  open(title, link) {
+    this._image.src = link;
+    this._image.alt = `Фото сделаное в ${title}`;
+    this._imageTitle.textContent = title;
+    super.open();
   }
 }
